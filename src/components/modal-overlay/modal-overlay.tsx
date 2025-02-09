8a4d7098ea226b1b86cbfc667edf08aa946d184e
Maksim Sergienko <creativesun@yandex.ru>
2025-02-09 23:54:30 +0300

    feat(modal-overlay): add modal-overlay

---
+++ src/components/modal-overlay/modal-overlay.tsx
@@ -0,0 +1,11 @@
+import styles from './style.module.scss';
+import { ModalOverlayProps } from './types';
+
+export const ModalOverlay = ({ onClose }: ModalOverlayProps) => {
+    return (
+        <div
+            className={styles.overlay}
+            onClick={onClose}
+        />
+    );
+};
