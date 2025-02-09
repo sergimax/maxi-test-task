5d4a09b6862bef9dc088aca3ea9627315aac8458
Maksim Sergienko <creativesun@yandex.ru>
2025-02-09 18:53:30 +0300

    feat(store): add selectors

--- src/app.tsx
+++ src/app.tsx
@@ -1,17 +1,31 @@
 import { useEffect } from 'react';
 import './app.css';
-import { useAppDispatch } from './services/hooks';
+import { useAppDispatch, useAppSelector } from './services/hooks';
 import { fetchUsers } from './services/reducers/users/thunks';
+import {
+    usersIsLoadedSelector,
+    usersIsLoadingSelector,
+    usersSelector,
+} from './services/reducers/users/selectors';
 
 function App() {
     const dispatch = useAppDispatch();
+    const users = useAppSelector(usersSelector);
+    const isLoading = useAppSelector(usersIsLoadingSelector);
+    const isLoaded = useAppSelector(usersIsLoadedSelector);
 
     useEffect(() => {
         console.log('APP');
         dispatch(fetchUsers());
     }, [dispatch]);
 
-    return <> Hello there!</>;
+    return (
+        <>
+            <h1>Hello there!</h1>
+            <div>users:</div>
+            {isLoaded && <div>{users[0].name}</div>}
+        </>
+    );
 }
 
 export default App;
---
+++ src/services/reducers/users/selectors.ts
@@ -0,0 +1,11 @@
+import { AppState } from '../../store';
+
+const rootSelector = (state: AppState) => state.model.users;
+
+export const usersIsLoadedSelector = (state: AppState) =>
+    rootSelector(state).isLoaded;
+export const usersIsLoadingSelector = (state: AppState) =>
+    rootSelector(state).isLoading;
+export const usersErrorSelector = (state: AppState) =>
+    rootSelector(state).error;
+export const usersSelector = (state: AppState) => rootSelector(state).users;
