7021caabe0b2852b0866c0231dc5320ad65909b3
Maksim <creativesun@yandex.ru>
2025-02-10 17:41:57 +0300

    chore(*): reformat utilities code

--- src/types/types.ts
+++ src/types/types.ts
@@ -2,8 +2,5 @@ import { JSX } from 'react';
 import { MODAL_TYPE } from '../constants/constants';
 
 export type DataForModal = {
-    title?: string;
-    content: JSX.Element;
-    type: MODAL_TYPE.CREATE_USER | MODAL_TYPE.DELETE_USER;
-    onAccept?: () => void;
+  title?: string; content: JSX.Element; type: MODAL_TYPE.CREATE_USER | MODAL_TYPE.DELETE_USER; onAccept?: () => void;
 };
